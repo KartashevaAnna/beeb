eb3d2022059b95bad502947c59baa86f88293c7a
Anna Veselova <annakartashevamail@gmail.com>
2025-01-27 12:56:20 +0300

    add welcome message

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
 import os
 
+import coloredlogs
 import uvicorn
+from loguru import logger
 
 from app.settings import SETTINGS
+from app.utils.welcome import WELCOME_MESSAGE
 
 if __name__ == "__main__":
+    coloredlogs.install(level=SETTINGS.server.log_level)
+    logger.opt(colors=True).success(f"<cyan>{WELCOME_MESSAGE}</>")
     uvicorn.run(
         "app.application:build_app",
         port=os.getenv("server_port", SETTINGS.server.port),
