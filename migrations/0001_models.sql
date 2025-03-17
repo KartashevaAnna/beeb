9c5a79af4e4b38b769a1925bf43bd0b313cc0786
Anna Veselova <annakartashevamail@gmail.com>
2025-03-17 11:40:55 +0300

    add timestamp to expenses

--- migrations/0001_models.sql
+++ migrations/0001_models.sql
@@ -1,5 +1,8 @@
+SET TIME ZONE 'Europe/Moscow';
+
 CREATE TABLE IF NOT EXISTS "main"."expenses" (
     "id" SERIAL PRIMARY KEY,
     "name" VARCHAR (255) NOT NULL,
-    "price" INT NOT NULL
+    "price" INT NOT NULL,
+    "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
