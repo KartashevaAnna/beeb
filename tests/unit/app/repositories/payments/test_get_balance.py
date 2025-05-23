78708312ec0500e776b2550aee366af1c7d17b58
Anna Veselova <annakartashevamail@gmail.com>
2025-05-23 07:28:31 +0300

    add current balance

---
+++ tests/unit/app/repositories/payments/test_get_balance.py
@@ -0,0 +1,105 @@
+from app.models import Payment
+from app.repositories.payments import PaymentRepo
+from tests.conftest import TEST_USER_ID, clean_db
+
+
+def test_get_balance(
+    session, current_payment, month_ago_payment, year_ago_payment, category
+):
+    salary = Payment(
+        name="зарплата",
+        price="20000",
+        user_id=TEST_USER_ID,
+        category_id=category.id,
+        is_spending=False,
+    )
+    session.add(salary)
+    session.flush()
+    session.commit()
+    spendings = (
+        current_payment.price + month_ago_payment.price + year_ago_payment.price
+    )
+    expected_result = salary.price - spendings
+    payments = [salary, current_payment, month_ago_payment, year_ago_payment]
+    obtained_result = PaymentRepo(session).get_balance(payments)
+    assert expected_result == obtained_result
+    clean_db(session)
+
+
+def test_get_balance_no_salary(
+    session, current_payment, month_ago_payment, year_ago_payment, category
+):
+    spendings = (
+        current_payment.price + month_ago_payment.price + year_ago_payment.price
+    )
+    expected_result = -spendings
+    payments = [current_payment, month_ago_payment, year_ago_payment]
+    obtained_result = PaymentRepo(session).get_balance(payments)
+    assert expected_result == obtained_result
+    clean_db(session)
+
+
+def test_get_balance_only_salary(session, category):
+    salary = Payment(
+        name="зарплата",
+        price="20000",
+        user_id=TEST_USER_ID,
+        category_id=category.id,
+        is_spending=False,
+    )
+    session.add(salary)
+    session.flush()
+    session.commit()
+
+    expected_result = salary.price
+    payments = [salary]
+    obtained_result = PaymentRepo(session).get_balance(payments)
+    assert expected_result == obtained_result
+    clean_db(session)
+
+
+def test_get_balance_two_salaries(
+    session,
+    category,
+    current_payment,
+    month_ago_payment,
+    year_ago_payment,
+):
+    salary = Payment(
+        name="зарплата",
+        price="20000",
+        user_id=TEST_USER_ID,
+        category_id=category.id,
+        is_spending=False,
+    )
+    session.add(salary)
+    session.flush()
+    session.commit()
+
+    second_salary = Payment(
+        name="зарплата",
+        price="5000",
+        user_id=TEST_USER_ID,
+        category_id=category.id,
+        is_spending=False,
+    )
+    session.add(salary)
+    session.flush()
+    session.commit()
+
+    income = int(salary.price) + int(second_salary.price)
+    spendings = (
+        current_payment.price + month_ago_payment.price + year_ago_payment.price
+    )
+    expected_result = income - spendings
+
+    payments = [
+        salary,
+        second_salary,
+        current_payment,
+        month_ago_payment,
+        year_ago_payment,
+    ]
+    obtained_result = PaymentRepo(session).get_balance(payments)
+    assert expected_result == obtained_result
+    clean_db(session)
